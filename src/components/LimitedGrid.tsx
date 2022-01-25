2afc2f3315851e804618587af540c8f09a9f40b8
Michał <sukowskimichal@gmail.com>
2022-01-25 16:49:57 +0100

    experimenting with different ideas

--- src/components/LimitedGrid.tsx
+++ src/components/LimitedGrid.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 const StyledSubgrid = styled.div`
@@ -6,13 +6,62 @@ const StyledSubgrid = styled.div`
     background: red;
     grid-column: 45/55;
     grid-row: 45/55;
-    grid-template-columns: subgrid;
-    grid-template-rows: subgrid;
+    grid-template-columns: repeat(10, 1fr);
+    grid-template-rows: repeat(10, 1fr);
+
+    a {
+        text-decoration: none;
+        width: 100%;
+        height: 100%;
+        background: yellow;
+        cursor: pointer;
+        border: 1px solid transparent;
+        transition: border 0.4s;
+
+        &:hover {
+            border: 1px solid blue;
+        }
+    }
+
+    .empty {
+        width: 100%;
+        height: 100%;
+
+        &:hover {
+            border: 1px solid yellow;
+        }
+    }
+
+    .limited {
+        position: relative;
+
+        &:hover::before {
+            content: "Edycja Limitowana"
+            position: absolute;
+            top: 0;
+            left: 0;
+            z-index: 5;
+        }
+    }
 `;
 
 const LimitedGrid = () => {
+    const [limitPixelPackages, setLimitPixelPackages] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/api/pixels/limited")
+        .then(res => res.json())
+        .then(data => setLimitPixelPackages(data))
+    }, []);
+
     return(
-        <StyledSubgrid />
+        <StyledSubgrid>
+            {limitPixelPackages.map((item: any) => {
+                return(
+                    item.isSold ? <a className="limited" href="/" style={{background: `${item.background}`}}><div></div></a> : <div className="empty limited"></div>
+                )
+            })}
+        </StyledSubgrid>
     );
 };
 
--- src/components/PixelsGrid.tsx
+++ src/components/PixelsGrid.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import LimitedGrid from './LimitedGrid';
 
@@ -13,14 +13,61 @@ const StyledGridContainer = styled.div`
     grid-template-columns: repeat(100, 1fr);
     grid-template-rows: repeat(100, 1fr);
     margin-top: 20px;
-    background: yellow;
+    position: relative;
+
+    &::before {
+        content: "";
+        position: absolute;
+        inset: 0;
+        background-image: url("images/earth.jpg");
+        background-size: cover;
+        opacity: 0.5;
+        z-index: -1;
+    }
+
+    a {
+        text-decoration: none;
+        width: 100%;
+        height: 100%;
+        background: yellow;
+        cursor: pointer;
+        border: 1px solid transparent;
+        transition: border 0.4s;
+
+        &:hover {
+            border: 1px solid blue;
+        }
+    }
+
+    .empty {
+        width: 100%;
+        height: 100%;
+    }
 `;
 
 const PixelsGrid = () => {
+    const [pixelPackages, setPixelPackages] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/api/pixels/nonlimited")
+        .then(res => res.json())
+        .then(data => setPixelPackages(data))
+    }, []);
+
     return(
-        <StyledGridContainer>
+        <>
+        {pixelPackages.length < 1 && <h2>Loading...</h2>}
+        {pixelPackages.length > 0 &&
+            <StyledGridContainer>
+            {pixelPackages.map((item: any) => {
+                return(
+                    item.isSold ? <a href="/" style={{background: `${item.background}`}}><div></div></a> : <div className="empty"></div>
+                )
+            })}
             <LimitedGrid />
         </StyledGridContainer>
+        }
+        </>
     );
 };
 
