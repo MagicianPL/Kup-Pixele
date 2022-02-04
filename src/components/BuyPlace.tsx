ea90492aaabc5fb0b74638623d8487f152dc9e2b
Michał <sukowskimichal@gmail.com>
2022-02-04 22:20:23 +0100

    BuyPlace in build

--- src/components/BuyPlace.tsx
+++ src/components/BuyPlace.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import CenteredContainer from './CenteredContainer';
+import Input from './Input';
 
 const StyledWrapper = styled.div`
     width: 100%;
@@ -18,10 +19,25 @@ const StyledWrapper = styled.div`
 `;
 
 const BuyPlace = () => {
+    const inputValues = {
+        qty: "",
+    }
+
+    const handleInputChange = () => {
+        console.log("mmk")
+    };
+
     return(
         <StyledWrapper>
         <CenteredContainer>
-            <h1>Strona w budowie</h1>
+            <h1>Kupno Miejsc</h1>
+            <form>
+                <Input id="name" name="name" label="Nazwa" placeholder="Podaj nazwę miejsca" value={inputValues.qty} onChange={handleInputChange} />
+                <Input id="url" name="url" label="Adres URL" value={inputValues.qty} placeholder="Adres www" onChange={handleInputChange} />
+                <Input id="description" name="description" label="Opis (opcjonalnie)" textarea={true} value={inputValues.qty} onChange={handleInputChange} />
+                <Input id="background" name="background" label="Kolor (w HEX kodzie)" value={inputValues.qty} onChange={handleInputChange} />
+                <Input id="qty" name="qty" label="Ilość miejsc" value={inputValues.qty} onChange={handleInputChange} />
+            </form>
         </CenteredContainer>
     </StyledWrapper>
     );
