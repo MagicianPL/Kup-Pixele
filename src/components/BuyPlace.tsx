01d45b79332c68feaf6a347c1fdd107ab0118efd
Michał <sukowskimichal@gmail.com>
2022-02-05 10:04:18 +0100

    colors section on build

--- src/components/BuyPlace.tsx
+++ src/components/BuyPlace.tsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import styled from 'styled-components';
+import { PixelsContext } from '../context/PixelsContext';
 import CenteredContainer from './CenteredContainer';
 import Input from './Input';
+import StyledButton from './StyledButton';
 
 const StyledWrapper = styled.div`
     width: 100%;
@@ -16,9 +18,60 @@ const StyledWrapper = styled.div`
             font-size: 26px;
         }
     }
+
+    .colors {
+        display: flex;
+        justify-content: center;
+        gap: 40px;
+        margin: 30px auto 15px auto;
+
+        div {
+            width: 100%;
+            max-width: 35px;
+            height: 100%;
+            aspect-ratio: 1/1;
+            border: 1px solid black;
+            cursor: pointer;
+            padding: 5px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border-radius: 4px;
+
+            &::after {
+                content: "";
+                width: 100%;
+                height: 100%;
+                display: block;
+            }
+
+            &:nth-child(1)::after {
+                background: #21092f;
+            }
+
+            &:nth-child(2)::after {
+                background: #a62508;
+            }
+
+            &:nth-child(3)::after {
+                background: #0da608;
+            }
+        }
+    }
+
+    a {
+        text-decoration: none;
+        color: inherit;
+        display: block;
+        text-align: center;
+        margin: 10px 0 30px 0;
+    }
 `;
 
 const BuyPlace = () => {
+    const places = useContext(PixelsContext);
+    const nonLimitedPlaces = places.filter((place: any) => place.isLimited === false && place.isSold === false);
+
     const initialValues = {
         name: "",
         url: "",
@@ -43,11 +96,18 @@ const BuyPlace = () => {
         <CenteredContainer>
             <h1>Kupno Miejsc</h1>
             <form>
-                <Input id="name" name="name" label="Nazwa" placeholder="Podaj nazwę miejsca" value={inputValues.name} onChange={handleInputChange} />
+                <Input id="name" name="name" label="Nazwa" placeholder="Podaj własną nazwę" value={inputValues.name} onChange={handleInputChange} />
                 <Input id="url" name="url" label="Adres URL" value={inputValues.url} placeholder="Adres www" onChange={handleInputChange} />
                 <Input id="description" name="description" label="Opis (opcjonalnie)" textarea={true} value={inputValues.description} onChange={handleInputChange} />
+                <div className="colors">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
                 <Input id="background" name="background" label="Kolor (w HEX kodzie)" value={inputValues.background} onChange={handleInputChange} />
-                <Input id="qty" name="qty" label="Ilość miejsc" value={inputValues.qty} onChange={handleInputChange} />
+                <a href="https://htmlcolorcodes.com/" target="_blank" rel="noreferrer">Generator kolorów</a>
+                <Input id="qty" name="qty" type="number" label={`Ilość miejsc ${places.length > 0 ? `(${nonLimitedPlaces.length} dostępnych z edycji nielimitowanej)` : ""}`} value={inputValues.qty} onChange={handleInputChange} />
+                <StyledButton primary>Kupuję</StyledButton>
             </form>
         </CenteredContainer>
     </StyledWrapper>
