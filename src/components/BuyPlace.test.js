ea85da954bbe8d6d8b12f136232c07738293b486
Michał <sukowskimichal@gmail.com>
2022-02-21 22:12:33 +0100

    second test

--- src/components/BuyPlace.test.js
+++ src/components/BuyPlace.test.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 import BuyPlace from "./BuyPlace";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
+import { UserContext } from "../context/UserContext";
 
 test("renders component on the screen", () => {
   render(
@@ -12,4 +14,22 @@ test("renders component on the screen", () => {
   const headerEl = screen.getByTestId("header");
 
   expect(headerEl.textContent).toBe("Kupno Miejsc");
+});
+
+test("inputs validation", async () => {
+  //Mocked UserContext data - user is true so he is logged
+  render(
+    <HashRouter>
+      <UserContext.Provider value={{ user: true }}>
+        <BuyPlace />
+      </UserContext.Provider>
+    </HashRouter>
+  );
+  const btn = screen.getByRole("button");
+  fireEvent.click(btn);
+  await waitFor(() => {
+    expect(
+      screen.getByText("Wszystkie wymagane pola muszą być wypełnione")
+    ).toBeInTheDocument();
+  });
 });
